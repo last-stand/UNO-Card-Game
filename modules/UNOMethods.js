6c44e745963a1edb56ecad936cf421f14f8ae6c7
Jai Prakash <jaiprak@thoughtworks.com>
2015-02-23 19:02:04 +0530

    addCards to player feature is created.

---
+++ modules/UNOMethods.js
@@ -0,0 +1,23 @@
+var fs = require("fs");
+var _ = require("lodash");
+
+var content = JSON.parse(fs.readFileSync('./public/cards.json'));
+var cards = Object.keys(content[0]);
+var shuffledCards = _.shuffle(cards);
+
+var players = [{p1:[]},{p2:[]},{p3:[]},{p4:[]}];
+
+var distributeCards = function(players){
+	var i = 1;
+	players.forEach(function(gamer){
+		gamer["p"+i] = shuffledCards.splice(0,7);
+		i++;
+	});
+}
+
+var addCards = function(players,player,shuffledCards, numberOfCards){
+	var cards = shuffledCards.splice(0,numberOfCards);
+	players.forEach(function(gamer){
+		(Object.keys(gamer)[0] == player) && (gamer[player] = gamer[player].concat(cards));
+	});
+}
