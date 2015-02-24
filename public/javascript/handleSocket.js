7c931f88d04aabc80aebb782f722c348a15079a2
Jai Prakash <jaiprak@thoughtworks.com>
2015-02-24 18:28:55 +0530

    Socket for client side is added.

---
+++ public/javascript/handleSocket.js
@@ -0,0 +1,50 @@
+var uno = require('unoBoard.js');
+
+// var  loadComments = function(){
+// 	$.ajax("/getComments/"+getId())
+// 		.done(function(data){
+// 			console.log(data);
+// 			var listed_data =getLists(data);
+// 			$("#allComments").html(listed_data);
+// 		});
+// }
+// var getLists = function(comments){
+// 	return comments.map(list).join("\r\n");
+// }
+// var list =function(comment){
+// 		return '<li><div><b>'+comment.name+":</b> "+comment.content+"<br>"+comment.time+"</div></li><br>";
+// }
+// var getId = function(){
+// 	var location = window.location.href.split('/');
+// 	var id = location[location.length-1];
+// 	return id;
+// }
+var onPageLoad =function(){
+	var socket = io.connect(window.location.hostname);
+
+	socket.on('new_content',function(data){
+		alert(data);
+		//generateCards(data);
+	 	// var comment=$("#mDiv").html();
+	 	// comment += list(data.comment);
+	 	// $("#mDiv").html(comment); 		
+	});
+	// socket.on('close',function(data){
+	//  	window.location.reload() 		
+	// });
+
+	// $("#btn_comment").click(function(){
+	// 	var id = getId();
+	// 	var content =$("#cmt_box").val();
+	// 	$("#cmt_box").val('');
+	// 	$.ajax({url:"/newComment/"+id,type:"POST", dataType: "json",data:{content:content}});
+	// });
+	
+	// $("#btn_loadComplete").click(loadComments)
+
+	// $("#cmt_box").keypress(function(e){
+	// 	e.keyCode == 13 && $("#btn_comment").click();
+	// })
+}
+
+$(onPageLoad);
