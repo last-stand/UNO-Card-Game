2a75781dc0bec2ff76c4132425814ba48ee58b7d
Surajit Barman <surajitb@thoughtworks.com>
2015-02-25 11:54:49 +0530

    password hashing functionality is done

--- tests/UnolibTest.js
+++ tests/UnolibTest.js
@@ -1,25 +1,16 @@
-var lib = require('../modules/UNOLib');
+var lib = require('../modules/lib.js').lib;
 var assert = require('chai').assert;
+var bc = require("bcryptjs");
 var fs = require('fs');
 var dbFileData = fs.readFileSync('tests/data/uno.db.backup');
 
-var uno_records;
-describe('uno_records',function(){
-	beforeEach(function(){
-		fs.writeFileSync('tests/data/uno.db',dbFileData);
-		uno_records = lib.init('tests/data/uno.db');
+describe('UNOLib',function(){
+	describe('#get_has_password',function(){
+		it('check the has property is working',function(done){
+			var password = "12345";
+			assert(12345!=lib.get_hash_password("12345"));
+			done();
+		})
 	});
-
-	// describe('#get_has_password',function(){
-	// 	it('check the has property is working',function(done){
-	// 		uno_records.getPlayerInfo("p1@gmail.com", function(err,player){
-	// 			assert.notOk(err);
-	// 			assert.equal(player.email,"p1@gmail.com");
-	// 			assert.equal(player.user_name,"p1");
-	// 		});
-	// 		done();
-	// 	});
-	// });
 	
-
 });
